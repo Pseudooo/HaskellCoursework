c3c438caad94af3663f4e50c5c2dac1e0693027a
pseudo <mitchell.wright877@gmail.com>
2020-03-18 20:26:23 +0000

    Updated Documentation

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,14 @@
 module Main where
 
-import System.IO -- Need hFlush to handle output/input order
-import Coursework
+-- Need `hFlush stdout` to handle inputs post compilation
+import System.IO
 
+import Coursework
 import Data
 
-{--}
-
+{-
+    Main function will load the data from the text file
+-}
 main :: IO ()
 main = do 
     -- Start with reading & parsing file 
@@ -14,11 +16,29 @@ main = do
     let places = map read $ lines contents :: [Place] 
     loop places
 
-
 loop :: [Place] -> IO ()
-loop [] = putStrLn "Goodbye!" -- Edge case to end loop
 loop places = do
 
+    -- Display possible options
+
+    putStr "Option: " -- Ask for an option
+    hFlush stdout
+    option <- getLine
+
+    -- Make any potential modifications to the data
+    let newData = process places option
+
+    -- Loop round with modified data
+    loop newData
+
+{-
+    Loop function will serve to continuously ask the user what they want
+    to do until they're done, at which point they're able to quit.
+-}
+loop :: [Place] -> IO ()
+loop places = do
+
+    putStrLn "" -- Padding
     putStrLn "Please select one of the following options to proceed:"
 
     -- Options that the user can perform
@@ -35,20 +55,23 @@ loop places = do
     -- Allow the user to quit the application
     if input == "q"
         then return ()
-        else do
+        else do -- Otherwise loop
             handle input places
             loop places
 
 
 {-
-    Function to handle different inputs
+    Function to handle each of the various options
 -}
 handle :: String -> [Place] -> IO ()
 
 -- List all places
 handle "1" places = do
     putStrLn "Places:"
-    mapM_ putStrLn $ getNames places 
+    mapM_ putStrLn $ getNames places
+
+-- If an invalid option is given
+handle _ _ = putStrLn "Invalid Option!"
 
 
 
